5f70d106ce54f09204b6ebf355b880455828c229
TimaGracia <timagracia9@gmail.com>
2021-10-19 13:02:27 -0300

    Validacion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "password",
+    "password": null,
     "database": "db_tripleg",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "password",
+    "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
