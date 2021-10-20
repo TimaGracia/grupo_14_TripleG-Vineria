7ebd165ffa2826010898285aad7ca6516d8d8c44
guillermoe3 <guillermojesq@gmail.com>
2021-10-20 18:02:16 -0300

    more fixes

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "password",
     "database": "db_tripleg",
     "host": "127.0.0.1",
     "dialect": "mysql"
